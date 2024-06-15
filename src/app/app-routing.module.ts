54692a7b2fb8c3fa9b88e78a9a3949aee1ffddc6
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-06-16 01:15:11 +0530

    implemented content page and uodated some ui of it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ContentComponent } from './content/content.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path : 'home',
     component:HomepageComponent
+  },
+  {
+    path : 'content',
+    component:ContentComponent
   }
 ];
 
